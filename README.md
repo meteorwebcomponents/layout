fa04dedb32d09c01ed4a9286b8457a6ec2298ca5
HedCET <linto.cet@gmail.com>
2015-11-16 11:30:41 +0530

    1'st commit

---
+++ README.md
@@ -0,0 +1,1 @@
+For [more](https://github.com/meteorwebcomponents/layout)
---
+++ package.js
@@ -0,0 +1,7 @@
+Package.describe({
+    documentation: "README.md",
+    git: "https://github.com/meteorwebcomponents/layout.git",
+    name: "mwc:polymer-layout",
+    summary: "mwc polymer layout",
+    version: "0.0.1"
+});
