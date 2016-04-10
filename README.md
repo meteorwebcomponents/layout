d54db9a33a7ff09f4e369132e68ed6406a9b6206
tkay <aruntknambiar@gmail.com>
2016-04-10 15:22:15 +0530

    meteor package works with meteor 1.3

--- README.md
+++ README.md
@@ -4,7 +4,28 @@
   -->
 # Meteor Webcomponents Layout.
 
+## Installation
+
+### Using meteor package. Meteor 1.3+. Depends on [synthesis](https://github.com/meteorwebcomponents/synthesis)
+
+```sh
+meteor add mwc:layout
+```
+### Using Bower
+
+```sh
+bower install mwc-layout --save
+```
+
+```html
+<link rel="import" href="bower_components/mwc-layout/mwc-layout.html">
+```
+
+> Note. Version 1.1.8 is for meteor 1.3+ . For meteor 1.2 use mwc-layout bower components
+
+
 ## Usage
+
 
 index.html
 
@@ -52,3 +73,4 @@ mwcLayout.render("demo-layout",{"main":"test-element"});
 Layout Manager for [Meteor](https://www.meteor.com/) + [Polymer - 1.0](https://www.polymer-project.org/) 
 
 Advanced Example - https://github.com/HedCET/TorrentAlert
+
--- package.js
+++ package.js
@@ -1,16 +1,19 @@
 Package.describe({
-    documentation: null,
-    git: "https://github.com/meteorwebcomponents/layout.git",
-    name: "mwc:layout",
-    summary: "mwc layout",
-    version: "1.1.7"
+  documentation: "README.md",
+  git: "https://github.com/meteorwebcomponents/layout.git",
+  name: "mwc:layout",
+  summary: "Polymer Layout renderer",
+  version: "1.1.8"
 });
 
 Package.onUse(function(api) {
-    api.versionsFrom("1.0");
+  api.versionsFrom("1.3");
+  api.use('ecmascript');
+  api.use('mwc:synthesis@1.0.12');
 
-    api.addAssets("mwc_layout.html", ["client"]);
-    api.addFiles("PolymerLayout.js", ["client"]);
+  api.addFiles("PolymerLayout.js", ["client"]);
+  api.addAssets("mwc_layout.html", ["client"]);
+  api.addFiles("mwc_layout.js", ["client"]);
 
-    api.export("mwcLayout");
+  api.export("mwcLayout",["client"]);
 });
