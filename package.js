7c42bdf0521c2a5cbb858fbd204fa7a0ac1f275d
tkay <aruntknambiar@gmail.com>
2016-05-25 18:32:05 +0530

    synth 1.0.24

--- package.js
+++ package.js
@@ -3,13 +3,13 @@ Package.describe({
   git: "https://github.com/meteorwebcomponents/layout.git",
   name: "mwc:layout",
   summary: "Polymer Layout renderer",
-  version: "1.1.15"
+  version: "1.1.16"
 });
 
 Package.onUse(function(api) {
   api.versionsFrom("1.3");
   api.use('ecmascript');
-  api.use('mwc:synthesis@1.0.22');
+  api.use('mwc:synthesis@1.0.24');
 
   api.addFiles("PolymerLayout.js", ["client"]);
   api.addFiles("mwc_layout.html", ["client"]);
