cfb75e845b24fafc2075d5717a0642e6b2eeffaf
aruntk <aruntknambiar@gmail.com>
2017-01-07 15:44:06 +0530

    synthesis 1.3.7

--- package.js
+++ package.js
@@ -3,13 +3,13 @@ Package.describe({
   git: "https://github.com/meteorwebcomponents/layout.git",
   name: "mwc:layout",
   summary: "Polymer Layout renderer",
-  version: "1.1.21"
+  version: "1.1.22"
 });
 
 Package.onUse(function(api) {
   api.versionsFrom("1.3");
   api.use('ecmascript');
-  api.use('mwc:synthesis@1.0.41');
+  api.use('mwc:synthesis@1.3.7');
 
   api.addFiles("PolymerLayout.js", ["client"]);
   api.addFiles("mwc_layout.html", ["client"]);
